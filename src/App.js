646d3d5305dba80ea7a872305cb7bff5fbd1206c
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-05-12 15:26:13 -0500

    Finished everything except annoying resolution changes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,47 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component {
 	constructor(){
 		super()
+
 		this.state = {
-			monsters: []
+			monsters: [],
+			searchField: ''
 		}
 	}
 	//ASYNC AWAIT VERSION
 	//Run when the component renders in the page (LIFECYCLE METHOD)
 	async componentDidMount() {
 		// //Make the API Call
-		// const response = await fetch('https://jsonplaceholder.typicode.com/users')
 		// //Convert into JSON
-		// const users = await response.json()
-		// //Use the data to render 
 		const users = await (
 			await fetch('https://jsonplaceholder.typicode.com/users')
 		).json()
 		//Send to this.state
 		this.setState({ monsters: users })
 	}
-	
-	// Promise Version
-	// //Run when the component renders in the page (LIFECYCLE METHOD)
-	// componentDidMount() {
-	// 	//Make the API Call
-	// 	fetch('https://jsonplaceholder.typicode.com/users')
-	// 	//Convert into JSON
-	// 	.then(response => response.json())
-	// 	//Use the data to render 
-	// 	.then(users => this.setState({ monsters: users }))
-	// }
 
+	handleChange = (event) => this.setState({ searchField: event.target.value })
+	
 	render () {
+		const { monsters, searchField } = this.state
+		const filteredMonsters = monsters.filter(monster=> 
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+			)
 		return (
 			<div className="App">
-				<CardList name='Marlrus'>
-				{
-					this.state.monsters.map(monster => 
-						<h1 key={monster.id}> {monster.name} </h1>
-					)
-				}
-				</CardList>
+				<h1> Monsters Rolodex </h1>
+				<SearchBox
+					placeholder='Search Monsters'
+					handleChange={this.handleChange}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		)
 	}
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
